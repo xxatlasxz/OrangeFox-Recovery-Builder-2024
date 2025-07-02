6d920863147dba1093097b0caca51815b5b58ea2
xxatlasxz <46904680+xxatlasxz@users.noreply.github.com>
2025-07-02 00:52:50 -0700

    Add files via upload

---
+++ austin/AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_austin.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_austin-user \
+    twrp_austin-userdebug \
+    twrp_austin-eng
